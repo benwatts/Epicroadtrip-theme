625909738e333e1cf3ea7aeea684e8c4ba4be4e1
Ben Watts <ben.watts@jadedpixel.com>
2011-08-05 14:18:37 -0600

    Missing older/newer post links. Pointed out by BC. LOL

--- home.php
+++ home.php
@@ -1,9 +1,15 @@
 <?php get_header(); ?>
 
   <div class="col-left">
-    <section id="excerpts">
+  
+  <section id="excerpts">
       <?php	get_template_part( 'loop', 'home' ); ?>	
     </section>
+
+		<div class="post-nav">
+	  <?php posts_nav_link(' / ','Newer Posts','Older Posts'); ?> 
+	  </div>
+	
   </div> 
   
   <div class="col-right">    
